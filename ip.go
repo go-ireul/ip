c4d1dad87e6ccdac13abefddf156baab646ac138
Yanke Guo <ryan@islandzero.net>
2017-11-02 05:01:59 +0800

    fix

--- ip.go
+++ ip.go
@@ -6,7 +6,7 @@ import "net"
 // reserved IP addresses are IP addresses you should not see from public internet
 // if you are building a service that sends requests on user's demand, you can use this method to check if user inputed a malicious IP
 func IsReservedIP(ip net.IP) bool {
-	for i, ipnet := range ReservedIPNets {
+	for _, ipnet := range ReservedIPNets {
 		if ipnet.Contains(ip) {
 			return true
 		}
